ddb1ff86230b47b2af2e3985b84fad3f5b388d7e
quydd <quydangdac@gmail.com>
2023-04-05 17:18:13 +0700

    Update Main activity

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.soloader.SoLoader;
+import com.reactnative.ivpusic.imagepicker.patch30277.PickerModule30277Workaround;
 
 public class MainActivity extends ReactActivity {
+
+   private final PickerModule30277Workaround mPickerModule30277Workaround = new PickerModule30277Workaround();
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -20,5 +23,23 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SoLoader.init(this, /* native exopackage */ false);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mPickerModule30277Workaround.onActivityResultTriggered(getReactInstanceManager(), requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPickerModule30277Workaround.onActivityResume(this, getReactInstanceManager());
+    }
+
+    @Override
+    protected void onStop() {
+         super.onStop();
+         mPickerModule30277Workaround.onActivityStop(getReactInstanceManager());
     }
 }
